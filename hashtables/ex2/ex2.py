070a17f6ad686a4747fc9a0ee202f71762d2a852
Ramon Audain <raudain123@gmail.com>
2020-04-10 11:49:14 -0700

    Complete ex2.py

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -18,26 +18,33 @@ class Ticket:
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
-
-    # The ticket for your first flight has a destination with a source of NONE
-    
-    # And the ticket for your final flight has a source with a destination of NONE.
     
     # Add destination to table
     for ticket in tickets:
         hash_table_insert(hashtable, ticket.source, ticket.destination)
-        
-    for trip in len(length):
-        # If flight is more than zero,
-        # subtract one flight from trip
-        if trip > 0:
-            # One less flight
-            flight = route[trip-1]
+    
+    
+    # Goes through the mulitple destinations
+    for trip in range(length):
+        # Adds NONE if trip has no destination
+        if trip <= 0:
+            start = "NONE"
         else:
-            flight = "NONE"
+            # Deletes index before and goes to next destination
+            start = route[trip-1]
 
-        hash_table_retrieve(hashtable, flight)
-        
+        # Retrives route from hash table
+        route[trip] = hash_table_retrieve(hashtable, start)
+    
+    # Return route without "NONE"
     return route[:-1]
+    
             
+# TESTING
+# ticket_1 = Ticket("NONE", "PDX")
+# ticket_2 = Ticket("PDX", "DCA")
+# ticket_3 = Ticket("DCA", "NONE")
 
+# tickets = [ticket_1, ticket_2, ticket_3]
+        
+# print(reconstruct_trip(tickets, 3))
